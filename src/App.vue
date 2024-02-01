e67dc1bee0f15accf42cd0d190e1b36e571aaff6
gabriarcu <38346968+gabriarcu@users.noreply.github.com>
2024-02-01 21:12:34 +0100

    2

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,46 @@
 <template>
-  <v-card flat>
-    <v-toolbar color="primary" dark extended flat>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      
+  <v-card color="grey-lighten-4" flat height="200px" rounded="1">
+    <v-toolbar color="primary" dark flat>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Portale</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
     </v-toolbar>
-    
-    
   </v-card>
+  <!-- <v-navigation-drawer v-model="drawer" temporary>
+    <v-list-item prepend-avatar="https://randomuser.me/api/portraits/men/78.jpg" title="John Leider"></v-list-item>
+
+    <v-divider></v-divider>
+
+    <v-list density="compact" nav>
+      <v-list-item prepend-icon="mdi-view-dashboard" title="Home" value="home"></v-list-item>
+      <v-list-item prepend-icon="mdi-forum" title="About" value="about"></v-list-item>
+    </v-list>
+  </v-navigation-drawer> -->
   <router-view />
 </template>
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+    }
+  },
+}
+</script>
 
 <style>
 #app {
--- src/views/ProdottiView.vue
+++ src/views/ProdottiView.vue
@@ -4,7 +4,7 @@
             <template v-slot:header="{ page, pageCount, prevPage, nextPage }">
                 <h1 class="text-h4 font-weight-bold d-flex justify-space-between mb-4 align-center">
                     <div class="text-truncate">
-                        Most popular mice
+                        Prodotti totali: {{ prodotti.length }}
                     </div>
 
                     <div class="d-flex align-center">
@@ -30,7 +30,7 @@
                             <v-img :src="item.raw.thumbnail" cover height="150"></v-img>
 
                             <v-list-item :title="item.raw.title" lines="two" density="comfortable"
-                                :subtitle="item.raw.description">
+                                :subtitle="item.raw.description.substring(0, 50)">
                                 <template v-slot:title>
                                     <strong class="text-h6">
                                         {{ item.raw.title }}
@@ -40,34 +40,78 @@
 
                             <v-table density="compact" class="text-caption">
                                 <tbody>
-                                    <tr align="right">
-                                        <th>Sconto:</th>
 
-                                        <td>{{ item.raw.discountPercentage }} %</td>
-                                    </tr>
 
                                     <tr align="right">
-                                        <th>Voto {{item.raw.rating  }}</th>
+                                        <th>Voto {{ item.raw.rating }}</th>
 
                                         <td><v-rating v-model="item.raw.rating" readonly half-increments></v-rating></td>
                                     </tr>
 
                                     <tr align="right">
-                                        <th>Weight:</th>
+                                        <th>Quantità:</th>
 
-                                        <td>{{ item.raw.rating }}</td>
+                                        <td>{{ item.raw.stock }}</td>
                                     </tr>
 
                                     <tr align="right">
-                                        <th>Wireless:</th>
+                                        <th>Categoria:</th>
 
-                                        <td>{{ item.raw.stock ? 'Yes' : 'No' }}</td>
+                                        <td>{{ item.raw.category }}</td>
                                     </tr>
 
+                                    <tr align="right">
+                                        <th>Sconto:</th>
+
+                                        <td>{{ item.raw.discountPercentage }} %</td>
+                                    </tr>
                                     <tr align="right">
                                         <th>Price:</th>
 
                                         <td>€ {{ item.raw.price }}</td>
+                                    </tr>
+                                    <tr align="right">
+                                        <td colspan="2">
+                                            <v-expansion-panels>
+                                                <v-expansion-panel>
+                                                    <v-expansion-panel-title>
+                                                        <template v-slot:default="{ expanded }">
+                                                            <v-row no-gutters>
+                                                                <v-col cols="4" class="d-flex justify-start">
+                                                                    Galleria
+                                                                </v-col>
+                                                                <v-col cols="8" class="text-grey">
+                                                                    <v-fade-transition leave-absolute>
+                                                                        <span v-if="expanded" key="0">
+                                                                        </span>
+                                                                        <span v-else key="1">
+
+                                                                        </span>
+                                                                    </v-fade-transition>
+                                                                </v-col>
+                                                            </v-row>
+                                                        </template>
+                                                    </v-expansion-panel-title>
+                                                    <v-expansion-panel-text>
+                                                        <v-row>
+                                                            <v-col>
+                                                                <v-carousel hide-delimiters progress="primary">
+                                                                    <v-carousel-item v-for="(item, i) in images[i]" :key="i"
+                                                                        :src="item" cover
+                                                                        style="width:auto;height:auto"></v-carousel-item></v-carousel>
+                                                            </v-col>
+                                                        </v-row>
+
+                                                        <v-divider></v-divider>
+
+                                                    </v-expansion-panel-text>
+                                                </v-expansion-panel>
+                                            </v-expansion-panels>
+                                        </td>
+                                    </tr>
+                                    <tr align="right">
+                                        <th></th>
+                                        <td></td>
                                     </tr>
                                 </tbody>
                             </v-table>
@@ -75,16 +119,6 @@
                     </v-col>
                 </v-row>
             </template>
-
-            <!-- <template v-slot:footer="{ page, pageCount }">
-            <v-footer color="surface-variant" class="justify-space-between text-body-2 mt-4">
-                Total mice: {{ prodotti.length }}
-
-                <div>
-                    Page {{ page }} of {{ pageCount }}
-                </div>
-            </v-footer>
-        </template> -->
         </v-data-iterator>
     </v-container>
 </template>
@@ -96,6 +130,7 @@ export default {
     data() {
         return {
             prodotti: [],
+            images: [[], []],
             itemsPerPage: 4,
         }
     },
@@ -103,9 +138,44 @@ export default {
         onClickSeeAll() {
             this.itemsPerPage = this.itemsPerPage === 4 ? this.prodotti.length : 4
         },
+        creaArrayImmagini() {
+            this.prodotti.forEach((prodotto) => {
+                // Inizializza un array per le immagini del prodotto corrente
+                const immaginiProdotto = [];
+
+                // Itera attraverso le immagini del prodotto corrente
+                prodotto.images.forEach((immagine) => {
+                    // Aggiungi l'immagine all'array delle immagini del prodotto corrente
+                    immaginiProdotto.push(immagine);
+                });
+
+                // Aggiungi l'array delle immagini del prodotto corrente a this.images
+                this.images.push(immaginiProdotto);
+            });
+            //elimina i primi due elementi dell'array
+            this.images.splice(0, 2);
+        }
+
     },
     mounted() {
         this.prodotti = prod;
+        this.creaArrayImmagini();
     }
+
+
+    // let i = 0;
+
+    // this.prodotti.forEach(element => {
+    //     let j = 0;
+    //     element.images.forEach(img => {
+    //         this.images[i][j] = img;
+    //         j++;
+    //     });
+    //     i++;
+    // });
+
 }
+
+
+
 </script>
